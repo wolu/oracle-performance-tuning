5dd47fc5325c40a985d30775bdfdf908abf59c37
Venkata Bhattaram <tinitiate@users.noreply.github.com>
2013-10-23 17:56:05 -0400

    Update Tuning.sql

--- Tuning.sql
+++ Tuning.sql
@@ -1,10 +1,14 @@
--- 
--- Oracle Performance Tuning
--- Venkata Bhattaram
--- Tinitiate.com
--- (c) on all CODE EXAMPLES
 --
-
+------------------------------------------------------------
+--
+-- Topic:      Oracle Performance tuning notes
+-- File Name:  oracle_terformance_tuning_notes.txt
+-- Author:     tinitiate.com, Venkata Bhattaram
+--             (c) on all CODE EXAMPLES
+-- Notes:      Writeup on some basics of performance tuning
+--
+------------------------------------------------------------
+--
 1) INTRODUCTION
    * Performance tuning or optimization is required to speedup the execution of DB operations.
    * It is job for Developers and DBAs at different magnitudes, Tuning is done at many levels depending 
@@ -99,47 +103,119 @@ Access paths are ways in which data is retrived from the Database.
 2) RowId Scans
    * RowId is the unique identifier in a Database for a Row.
    * It spefcifies the datafile, data block of the row.
-   * Optimized does a RowId scan when a index scan happens
+   * Optimizer does a RowId scan when a index scan happens
+
+3) Index Scans
    
-      
+   * All index scans happen at the BLOCK level, IO   
+   
+   Index Unique Scans (Using PK /UK /UI)
+      unique scan if a statement contains a UNIQUE or a PRIMARY KEY constraint that
+      guarantees that only a single row is accessed.
+   
+   Index Range Scans
+      * Avoid sort with Index scans
+      * Indexes are by default saved ASC order,
+      * Use Index Range Scans Descending, to change sort order
+      * Leading_col1 = :b1
+        Leading_col1 < :b1
+        Leading_col1 > :b1
+      * DOES NOT WORK with Leading Column like '%MyValue%'  
+   Index Skip Scans: Join NOT having the Leading indexed column, but the next column..
+   Index Full Scans: Using ORDER BY on an Indexed column, Non Parallel, scans full Index
+   Fast Full Index Scans
+      * Alternative to a full table scan when the index contains all the columns
+        that are needed for the query
+      * use hint index_ffs, Can be run in parallel
+      * Does mulit-block reads
+
+================
+== JOIN TYPES ==
+================
+Nested Loop
+   Small Table is the driver and looped over the big table
+Hash Join
+   Creates a hash table of the smaller table and does a nested loop
+   only works where there is an = equal to.
+Sort-Merge
+   As the name indicates, sort both inputs in a order and merge where its equal, for all the interleaved
+   in smaller table ignore, sorting is the most expensive part.
+
+
 ===================================
 ==  DB Initialization Parameters ==
 ===================================
+   OPTIMIZER_FEATURES_ENABLE
+      The OPTIMIZER_FEATURES_ENABLE initialization parameter enables 
+      optimizer-features, for a specific release. Using release numbers like 10.2.0.1, 11.2.0.1
+   SET_AORT_AREA_SIZE
+   PLSQL_OPTIMIZE_LEVEL
+   OPTIMIZER_MODE
+      ALL_ROWS
+         Cost Based Optimizer tries for the of best throughput, using minimal resources
+         use to complete the entire statement, This is the default value for this Parameter.
+      FIRST_ROWS / FIRST_ROWS_n
+         Cost Based Optimizer, regardless of statistics tries for the best response time 
+         to return the first n/Few rows
+   STAR_TRANSFORMATION_ENABLED
+      If set TRUE, the optimizer for star queries, combines the FACT tables 
+      bitmap indexes on the various columns.
 
-
-
+   
 3) SQL tuning where to start ?
    * In order to fix something we need to understand what the problem is,
      Oracle provides with the various tools and options to dig through the SQL execution plan.
 
-   ===============================
-   = Explain Plan with PlanTable =
-   ===============================
-      1) Create the ExplainPlan
+===============================
+= Explain Plan with PlanTable =
+===============================
+      1) Create the ExplainPlan using the command:
 
-      Run the Command:
-         EXPLAIN PLAN FOR
-         select * from MyQuery;
+        SQL>  EXPLAIN PLAN FOR
+              select * from MyQuery;
 
-      OR, to ID the plan
+      2) Create an Explain Plan with a PlanID:
 
-         EXPLAIN PLAN
-         SET STATEMENT_ID = 'st1'
-         FOR
-         SELECT last_name FROM MyQuery;   
+        SQL> EXPLAIN PLAN
+             SET STATEMENT_ID = 'st1'
+             FOR
+             SELECT last_name FROM MyQuery;   
 
       2) View the explain plan
 
-         SELECT PLAN_TABLE_OUTPUT FROM TABLE(DBMS_XPLAN.DISPLAY());
+         SQL> SELECT PLAN_TABLE_OUTPUT FROM TABLE(DBMS_XPLAN.DISPLAY());
       
       3) Understanding the explain plan:
-      
+
+=====================================
+== dbms_sqltune.report_sql_monitor ==
+=====================================
+
+   -- Get the SQL ID for your statement
+   -- Works in 11g
+   --
+   select  /*+ first_rows */ s.sql_text,m.sql_id, m.SQL_EXEC_ID, m.sql_exec_start
+   -- into    l_sql_text,l_sql_id, l_SQL_EXEC_ID, l_sql_exec_start
+   from    v$sql                     s
+          ,v$sql_plan_monitor        m
+   where  m.sql_id = s.sql_id
+   -- and    s.sql_id = &SQL_ID      -- GET YOUR SQLs SQL_ID
+   -- and    s.status = 'EXECUTING'  -- STATUS is Executing
+
+   -- VIEW THE REPORT
+   --   
+   SELECT dbms_sqltune.report_sql_monitor( sql_id         => :l_sql_text
+                                          ,sql_exec_id    => :sql_exec_id
+                                          ,sql_exec_start => :sql_exec_start
+                                          ,report_level   => 'ALL' )
+   FROM   dual;
+   
 
 
 
-   =============
-   = AutoTrace =
-   =============
+=============
+= AutoTrace =
+=============
 
    ========================================================
    = GATHER_PLAN_STATISTICS and V$SQL_PLAN_STATISTICS_ALL =
